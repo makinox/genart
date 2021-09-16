ec1af3677891ea5d63bc246df008229da132d827
Jesus Bossa <x@jesusbossa.dev>
2021-09-15 19:37:48 -0500

    feat: random gaussian

--- sketch.js
+++ sketch.js
@@ -9,19 +9,22 @@ const settings = {
 const sketch = () => {
   function createGrid() {
     const points = [];
-    const count = 40;
+    const count = 30;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        points.push([u, v]);
+        points.push({
+          radius: Math.abs(random.gaussian() * 0.01),
+          position: [u, v],
+        });
       }
     }
     return points;
   }
 
-  random.setSeed(511);
+  random.setSeed(512);
   const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 400;
 
@@ -29,15 +32,16 @@ const sketch = () => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
-    points.forEach(([u, v]) => {
+    points.forEach((data) => {
+      const { position, radius } = data;
+      const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
       context.beginPath();
-      context.arc(x, y, 5, 0, Math.PI * 2, false);
-      context.strokeStyle = 'black';
-      context.lineWidth = 20;
-      context.stroke();
+      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      context.fillStyle = 'red';
+      context.fill();
     });
   };
 };
