c78d552f01b05b76963b2a2bcf279b44aec49d62
Jesus Bossa <x@jesusbossa.dev>
2021-09-16 20:46:52 -0500

    chore: save random seed

--- sketch.js
+++ sketch.js
@@ -3,28 +3,33 @@ const { lerp } = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 const palettes = require('nice-color-palettes');
 
+random.setSeed(random.getRandomSeed());
+
 const settings = {
+  suffix: random.getRandomSeed(),
   dimensions: [2048, 2048],
 };
+
+console.log(random.getSeed());
 
 const sketch = () => {
   const colorCout = random.rangeFloor(1, 6);
   const palette = random.shuffle(random.pick(palettes)).slice(0, colorCout);
-  console.log({ palette });
 
   function createGrid() {
     const points = [];
-    const count = 30;
+    const count = 50;
 
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.05;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.1;
 
         points.push({
           color: random.pick(palette),
           radius: radius,
+          rotation: random.noise2D(u, v),
           position: [u, v],
         });
       }
@@ -41,15 +46,23 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     points.forEach((data) => {
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation } = data;
       const [u, v] = position;
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Arial"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('=', 0, 0);
+      context.restore();
     });
   };
 };
