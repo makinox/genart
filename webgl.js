8768007c62d7f11eb6d2913799e9fd7d155e43fd
Jesus Bossa <x@jesusbossa.dev>
2021-09-21 09:10:10 -0500

    chore: making multiple cubes

--- webgl.js
+++ webgl.js
@@ -1,5 +1,6 @@
 // Ensure ThreeJS is in global scope for the 'examples/'
 global.THREE = require('three');
+const random = require('canvas-sketch-util/random');
 
 // Include any additional ThreeJS examples below
 require('three/examples/js/controls/OrbitControls');
@@ -21,15 +22,15 @@ const sketch = ({ context }) => {
   });
 
   // WebGL background color
-  renderer.setClearColor('#000', 1);
+  renderer.setClearColor('hsl(0, 0%, 95%)', 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(30, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
-  camera.lookAt(new THREE.Vector3());
+  const camera = new THREE.OrthographicCamera();
+  // camera.position.set(2, 2, -4);
+  // camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
-  const controls = new THREE.OrbitControls(camera, context.canvas);
+  // const controls = new THREE.OrbitControls(camera, context.canvas);
 
   // Setup your scene
   const scene = new THREE.Scene();
@@ -42,25 +43,23 @@ const sketch = ({ context }) => {
   //   color: 'red',
   //   wireframe: true,
   // });
+
   // Setup a geometry
   const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-  // Setup a material
-  const material = new THREE.MeshPhysicalMaterial({
-    color: 'red',
-    roughness: 1,
-    flatShading: true,
-  });
+  for (let index = 0; index < 10; index++) {
+    // Setup a material
+    const material = new THREE.MeshBasicMaterial({
+      color: 'red',
+    });
 
-  // Setup a mesh with geometry + material
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+    // Setup a mesh with geometry + material
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(random.range(-1, 1), random.range(-1, 1), random.range(-1, 1));
+    mesh.scale.multiplyScalar(0.1);
 
-  scene.add(new THREE.AmbientLight('#59314f'));
-
-  const ligth = new THREE.PointLight('#45caf7', 1, 15.5);
-  ligth.position.set(2, 2, -4).multiplyScalar(1.5);
-  scene.add(ligth);
+    scene.add(mesh);
+  }
 
   // draw each frame
   return {
@@ -68,18 +67,40 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
-      camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 1.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
+      camera.updateProjectionMatrix();
+
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
     render({ time }) {
-      mesh.rotation.y = time * ((10 * Math.PI) / 180);
-      controls.update();
+      // mesh.rotation.y = time;
+      // controls.update();
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
     unload() {
-      controls.dispose();
+      // controls.dispose();
       renderer.dispose();
     },
   };
