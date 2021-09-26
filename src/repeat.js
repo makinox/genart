e7f67798bc8678321ac2598e12d6f5ba234b4809
Jesus Bossa <x@jesusbossa.dev>
2021-09-25 22:54:28 -0500

    chore: noise random

--- src/repeat.js
+++ src/repeat.js
@@ -5,6 +5,7 @@ global.THREE = require('three');
 require('three/examples/js/controls/OrbitControls');
 
 const canvasSketch = require('canvas-sketch');
+const glsl = require('glslify');
 
 const settings = {
   // Make the loop animated
@@ -45,7 +46,9 @@ const sketch = ({ context }) => {
     }
   `;
 
-  const fragmentShader = /* glsl */ `
+  const fragmentShader = glsl(/* glsl */ `
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+
   varying vec2 vUv;
   uniform vec3 color;
   uniform float time;
@@ -59,14 +62,18 @@ const sketch = ({ context }) => {
 
       float d = distance(pos, center);
 
-      float mask = step(0.25 + sin(time  + vUv.x * 2.0) * 0.25, d);
+      // float mask = step(0.25 + sin(time  + vUv.x * 2.0) * 0.25, d);
+
+      vec2 noiseInput = floor(q * 10.0);
+      float offset = noise(vec3(noiseInput.xy, time)) * 0.25;
+      float mask = step(0.25 + offset, d);
       mask = 1.0 - mask;
 
       vec3 mixColor = mix(color, vec3(1.0), mask);
 
       gl_FragColor = vec4(vec3(mixColor), 1.0);
     }
-  `;
+  `);
 
   // Setup a material
   const material = new THREE.ShaderMaterial({
